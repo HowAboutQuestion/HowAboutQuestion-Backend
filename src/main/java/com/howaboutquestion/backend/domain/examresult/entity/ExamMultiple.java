d0deb8df1d673a6796de496c9704caec87bed6c3
khael <khaelim1311@gmail.com>
2025-07-27 21:31:31 +0900

    feat: Mapping examresult domain

---
+++ src/main/java/com/howaboutquestion/backend/domain/examresult/entity/ExamMultiple.java
@@ -0,0 +1,47 @@
+
+package com.howaboutquestion.backend.domain.examresult.entity;
+
+import com.howaboutquestion.backend.domain.question.entity.MultipleAnswer;
+import jakarta.persistence.*;
+import lombok.*;
+
+/**
+ * packageName    : com.howaboutquestion.backend.domain.examresult.entity<br>
+ * fileName       : ExamMultiple.java<br>
+ * author         : khaelim1311 <br>
+ * date           : 2025-07-24<br>
+ * description    : ExamMultiple entity 클래스입니다. <br>
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 25.07.24          khaelim1311         최초생성<br>
+ */
+@Entity
+@Getter
+@Builder
+@Table(name = "tb_exam_multiple")
+@PrimaryKeyJoinColumn(name = "id")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ExamMultiple extends ExamResultEntity {
+
+    @Column(name = "select_one", length = 255, nullable = false)
+    private String selectOne;
+
+    @Column(name = "select_two", length = 255)
+    private String selectTwo;
+
+    @Column(name = "select_three", length = 255)
+    private String selectThree;
+
+    @Column(name = "select_four", length = 255)
+    private String selectFour;
+
+    @Column(name = "select_five", length = 255)
+    private String selectFive;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 255)
+    private MultipleAnswer answer;
+
+}
---
+++ src/main/java/com/howaboutquestion/backend/domain/examresult/entity/ExamSubjective.java
@@ -0,0 +1,27 @@
+package com.howaboutquestion.backend.domain.examresult.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+/**
+ * packageName    : com.howaboutquestion.backend.domain.examresult.entity<br>
+ * fileName       : ExamSubjective.java<br>
+ * author         : khaelim1311 <br>
+ * date           : 2025-07-24<br>
+ * description    : ExamSubjective entity 클래스입니다. <br>
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 25.07.24          khaelim1311         최초생성<br>
+ */
+@Entity
+@Getter
+@Builder
+@PrimaryKeyJoinColumn(name = "id")
+@Table(name = "tb_exam_subjective")
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ExamSubjective extends ExamResultEntity {
+
+    @Column(nullable = false)
+    private String answer;
+}
