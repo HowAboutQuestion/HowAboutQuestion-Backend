4b0b73c0dfe37889f2ddc96ac1f7bb8afc89748c
khael <khaelim1311@gmail.com>
2025-07-27 21:32:37 +0900

    feat: Mapping subscribe domain

---
+++ src/main/java/com/howaboutquestion/backend/domain/subscribe/entity/SubscribeEntity.java
@@ -0,0 +1,61 @@
+package com.howaboutquestion.backend.domain.subscribe.entity;
+
+import com.howaboutquestion.backend.domain.book.entity.BookEntity;
+import com.howaboutquestion.backend.domain.usermeta.entity.UserMetaEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+/**
+ * packageName    : com.howaboutquestion.backend.domain.subscribe.entity<br>
+ * fileName       : SubscribeEntity.java<br>
+ * author         : khaelim1311 <br>
+ * date           : 2025-07-24<br>
+ * description    : Subscribe Entity 클래스입니다. <br>
+ * ===========================================================<br>
+ * DATE              AUTHOR             NOTE<br>
+ * -----------------------------------------------------------<br>
+ * 25.07.24          khaelim1311         최초생성<br>
+ */
+@Entity
+@Getter
+@Builder
+@Table(name = "tb_subscribe")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SubscribeEntity {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, columnDefinition = "INT UNSIGNED")
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "book_id")
+    private BookEntity book;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserMetaEntity user;
+
+    @Column(name = "check_favorite", nullable = false)
+    private Boolean checkFavorite;
+
+    @PrePersist
+    public void prePersist() {
+        if (checkFavorite == null) {
+            this.checkFavorite = Boolean.FALSE;
+        }
+    }
+}
+/**
+ *
+ *
+ *   `id` int unsigned NOT NULL AUTO_INCREMENT,
+ *   `book_id` int unsigned NOT NULL,
+ *   `user_id` int unsigned NOT NULL,
+ *   `check_favorite` tinyint(1) NOT NULL DEFAULT '0',
+ *
+ */
