803086123f47a399d060ebb5b25a8183d8af84c3
cod0216 <cod0216@nate.com>
2026-05-06 17:25:04 +0900

    feat: add exam answer and result endpoints

--- src/main/java/com/howaboutquestion/backend/domain/exam/controller/ExamController.java
+++ src/main/java/com/howaboutquestion/backend/domain/exam/controller/ExamController.java
@@ -1,5 +1,6 @@
 package com.howaboutquestion.backend.domain.exam.controller;
 
+import com.howaboutquestion.backend.domain.exam.dto.request.ExamAnswerSubmitRequest;
 import com.howaboutquestion.backend.domain.exam.dto.request.ExamStartRequest;
 import com.howaboutquestion.backend.domain.exam.service.ExamComplexService;
 import com.howaboutquestion.backend.domain.user.dto.UserDetail;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  * DATE              AUTHOR             NOTE<br>
  * -----------------------------------------------------------<br>
  * 26.05.06          eunchang          최초 생성<br>
+ * 26.05.06          eunchang          답안 제출 및 결과 조회 엔드포인트 추가<br>
  */
 @RestController
 @RequiredArgsConstructor
@@ -39,6 +43,28 @@ public class ExamController {
         return ResponseUtility.success(
                 examComplexService.startExam(Long.parseLong(userDetail.getUserId()), request),
                 "시험이 시작되었습니다."
+        );
+    }
+
+    @PostMapping("/{examId}/answer")
+    public ResponseEntity<?> submitAnswers(
+            @AuthenticationPrincipal UserDetail userDetail,
+            @PathVariable Integer examId,
+            @RequestBody @Valid ExamAnswerSubmitRequest request
+    ) {
+        return ResponseUtility.success(
+                examComplexService.submitAnswers(Long.parseLong(userDetail.getUserId()), examId, request),
+                "답안이 제출되었습니다."
+        );
+    }
+
+    @GetMapping("/{examId}/result")
+    public ResponseEntity<?> getExamResult(
+            @AuthenticationPrincipal UserDetail userDetail,
+            @PathVariable Integer examId
+    ) {
+        return ResponseUtility.success(
+                examComplexService.getExamResult(Long.parseLong(userDetail.getUserId()), examId)
         );
     }
 }
